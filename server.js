1781aa8084135880e0640c3642799b6f9741ceb6
chriswongatcuhk <wc38.wong@gmail.com>
2019-06-04 12:52:23 +0800

    Test curl

--- server.js
+++ server.js
@@ -2,9 +2,28 @@
 const express = require('express'); 
 const path = require('path'); 
 const app = express(); 
+
+var child_process = require('child_process');
+
+function runCmd(cmd)
+{
+  var resp = child_process.execSync(cmd);
+  var result = resp.toString('UTF8');
+  return result;
+}
+
 // Serve static files…. 
 app.use(express.static(__dirname + '/dist/angular7-express')); 
 // Send all requests to index.html 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/dist/angular7-express/index.html')); });
+
+app.get('/curl-test', (req, res) => { 
+    
+    var cmd = "curl https://y03vvyzq2v.sse.codesandbox.io/products/Zgz9gZE";  
+    var result = runCmd(cmd);
+
+    res.end(result); 
+});
+
 // default Heroku PORT 
 app.listen(process.env.PORT || 3000);
